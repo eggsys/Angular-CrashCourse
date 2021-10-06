cec9a9ae9fa186857a80a2be996f8190c7830e58
eggsys <46551235+eggsys@users.noreply.github.com>
2021-10-06 16:03:57 +0700

    appmodule + http , task service from API

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+import { Observable} from 'rxjs';
 import { Task } from '../Task'    // interface
-import { TASKS } from '../mock-tasks' // mock data
+//import { TASKS } from '../mock-tasks' // mock data
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks;
+    //const tasks = of(TASKS)
+    //return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
